fd3495f32d77ce7ca87110810922e5df4e916dc9
Benjamin Behringer <benjamin.behringer@me.com>
2022-06-07 13:15:15 +0200

    add wireguard and eu-central-1

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -368,6 +368,16 @@ echo vm.max_map_count=524288 | sudo tee -a /etc/sysctl.conf
 
 
 ################################################################################
+### Install Wireguard ##########################################################
+################################################################################
+
+sudo yum install kernel-devel-`uname -r` -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+sudo curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+sudo yum update -y
+sudo yum install wireguard-dkms wireguard-tools -y
+
+################################################################################
 ### Cleanup ####################################################################
 ################################################################################
 
